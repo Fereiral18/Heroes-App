17cc741ba7bc8f894224c670099757f8415e59e0
Luis Fereira <luis@spartandevs.com>
2023-02-16 17:00:01 -0400

    [LFereira]add new Router, Login router and eliminate navBar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,17 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
     let activeStyle = {
         textDecoration: "underline",
       };
-    
+      const navigate =  useNavigate()
+     
+    const onLogout = () =>{
+        navigate('/login',{
+        replace: true
+    })
+    }
     
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -45,7 +51,8 @@ export const Navbar = () => {
                         Luis fereira
                     </span>
                     <button
-                    className='nav-item nav-link btn'>
+                    className='nav-item nav-link btn'
+                    onClick={onLogout}>
                         Logout
                     </button>
                 </ul>
