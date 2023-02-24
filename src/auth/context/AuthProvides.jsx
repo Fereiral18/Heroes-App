2f3322bed7870f7e2520ba94636847f0652111aa
Luis Fereira <luis@spartandevs.com>
2023-02-24 14:55:01 -0400

    [LFereira] add localStorage, saves users

--- src/auth/context/AuthProvides.jsx
+++ src/auth/context/AuthProvides.jsx
@@ -1,18 +1,45 @@
 import { useReducer } from "react"
+import { types } from "../types/types"
 import { AuthContext } from "./AuthContext"
 import { AuthReducer } from "./AuthReducer"
 
 
-const initialState = {
-    logged: false,
+
+
+const init = () => {
+  const user= JSON.parse(localStorage.getItem('user'))
+  return{
+    logged: !!user,
+    user
+  }
 }
 
 export const AuthProvides = ({children}) => {
 
-   const [authState, dispatch] = useReducer(AuthReducer, initialState)
+   const [authState, dispatch] = useReducer(AuthReducer, {}, init)
+
+   const login = async (name = '') =>{
+
+    const user = { id: 'ABC', name}
+
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'ABC',
+        name: name
+      }
+    }
+
+    localStorage.setItem('user', JSON.stringify( user ))
+
+    dispatch(action)
+   }
 
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{
+      ...authState,
+      login: login
+    }}>
         { children }
     </AuthContext.Provider>
   )
