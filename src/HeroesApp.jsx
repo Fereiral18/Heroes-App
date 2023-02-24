7e5d7e5dbfca51ebc7dcb82f61dfd0e8de16ba3e
Luis Fereira <luis@spartandevs.com>
2023-02-24 12:54:24 -0400

    [LFereira] adds useReducer, AuthContext, AuthProvides, AuthReducer

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,10 @@
+import { AuthProvides } from "./auth";
 import { AppRouter } from "./router/AppRouter";
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvides>
      <AppRouter/>
-    </>
+    </AuthProvides>
   );
 };
---
+++ src/auth/context/index.js
@@ -0,0 +1,3 @@
+export * from './AuthContext'
+export * from './AuthProvides'
+export * from './AuthReducer'
